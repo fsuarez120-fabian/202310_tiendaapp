5a02cbf7d63f966b7662568379c2f70cce7764f5
Fabian Enrique Suarez Carvajal <fsuarez120@unab.edu.co>
2023-04-18 16:04:47 -0500

    Firebase se estan guardando los datos en firebase cuando se crea los datos.

--- app/src/main/java/com/example/suarez_carvajal/FormularioActivity.java
+++ app/src/main/java/com/example/suarez_carvajal/FormularioActivity.java
@@ -2,13 +2,41 @@ package com.example.suarez_carvajal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class FormularioActivity extends AppCompatActivity {
+
+    EditText etNombre, etPrecio, etUrlImagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        etNombre = findViewById(R.id.et_nombre_formulario);
+        etPrecio = findViewById(R.id.et_precio_formulario);
+        etUrlImagen = findViewById(R.id.et_imgen_formulario);
+
+    }
+
+    public void clickGuardar(View view){
+
+        String nombre = etNombre.getText().toString();
+        Double precio = Double.parseDouble(etPrecio.getText().toString());
+        String urlImagen = etUrlImagen.getText().toString();
+
+        Producto newProduct = new Producto(nombre,precio,urlImagen);
+
+        //guardarlo en FireStore
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.collection("productos").add(newProduct);
+
+        finish();
+        Toast.makeText(this, "SE GUARDO EL PRODUCTO", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/suarez_carvajal/Producto.java
+++ app/src/main/java/com/example/suarez_carvajal/Producto.java
@@ -1,5 +1,7 @@
 package com.example.suarez_carvajal;
 
+import com.google.firebase.firestore.PropertyName;
+
 import java.io.Serializable;
 
 public class Producto implements Serializable {
@@ -7,6 +9,7 @@ public class Producto implements Serializable {
     private Double precio;
     private String urlImagen;
 
+    //para conectar Firebase se debe tener un constructor vacio y los gettes y setters
     public Producto(){}
 
     public Producto(String nombre, Double precio, String urlImagen) {
@@ -14,6 +17,8 @@ public class Producto implements Serializable {
         this.precio = precio;
         this.urlImagen = urlImagen;
     }
+
+    //@Exclude es para ignorar en Firebase pero debe ser en los getters y setter
 
     public String getNombre() {
         return nombre;
@@ -27,9 +32,11 @@ public class Producto implements Serializable {
     public void setPrecio(Double precio) {
         this.precio = precio;
     }
+    @PropertyName("url_imagen")
     public String getUrlImagen() {
         return urlImagen;
     }
+    @PropertyName("url_imagen")
     public void setUrlImagen(String urlImagen) {
         this.urlImagen = urlImagen;
     }
