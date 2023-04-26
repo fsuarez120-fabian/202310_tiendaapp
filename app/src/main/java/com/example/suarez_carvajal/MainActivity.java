fb83d3decffbfd3f6b81df5ed37632eb9b73406b
Fabian Enrique Suarez Carvajal <fsuarez120@unab.edu.co>
2023-04-25 20:01:11 -0500

    Firebase se estan consultando los datos de firebase para que se muestre el listado.

--- app/src/main/java/com/example/suarez_carvajal/MainActivity.java
+++ app/src/main/java/com/example/suarez_carvajal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.suarez_carvajal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,12 +11,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<Producto> listaPrincipalProductos;
+    private ArrayList<Producto> listaPrincipalProductos = new ArrayList<>();;
     private RecyclerView rvListadoProductos;
+    private AdaptadorPersonalizado miAdaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +31,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle(getString(R.string.txt_listado));
 
-        cargarDatos();
+        //cargarDatos();
 
         rvListadoProductos = findViewById(R.id.rv_listado_productos);
 
-        AdaptadorPersonalizado miAdaptador = new AdaptadorPersonalizado(listaPrincipalProductos);
+        miAdaptador = new AdaptadorPersonalizado(listaPrincipalProductos);
 
         miAdaptador.setOnItemClickListener(new AdaptadorPersonalizado.OnItemClickListener() {
             @Override
@@ -50,8 +58,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        cargarDatos();
+    }
+
     public void cargarDatos() {
-        // Crear productos
+
+        //guardarlo en FireStore
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        firestore.collection("productos").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isComplete()){
+                    listaPrincipalProductos.clear();
+                    for (DocumentSnapshot myDocumento : task.getResult()){
+                        Producto myProduct = myDocumento.toObject(Producto.class);
+                        listaPrincipalProductos.add(myProduct);
+                    }
+                    miAdaptador.setListadoInfomacion(listaPrincipalProductos);
+                }else{
+                    Toast.makeText(MainActivity.this, "No se pudo conectar con el servidor.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+        /*// Crear productos
         Producto producto1 = new Producto();
         producto1.setNombre("Computador HP");
         producto1.setPrecio(8000000.0);
@@ -76,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         listaPrincipalProductos.add(producto3);
         listaPrincipalProductos.add(producto1);
         listaPrincipalProductos.add(producto2);
-        listaPrincipalProductos.add(producto3);
+        listaPrincipalProductos.add(producto3);*/
 
     }
 
